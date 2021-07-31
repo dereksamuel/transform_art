65aae1851f67a1a41e528108913b311941e20c47
Derek Paul <dereksamuelgr@gmail.com>
2021-07-31 17:21:31 -0500

    my first commit

---
+++ src/helpers/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+let firebaseConfig = {
+  apiKey: "AIzaSyCfDf9nW0LOeywckYPI92ql2OrEgeAo25g",
+  authDomain: "transformart-5e776.firebaseapp.com",
+  projectId: "transformart-5e776",
+  storageBucket: "transformart-5e776.appspot.com",
+  messagingSenderId: "82195915685",
+  appId: "1:82195915685:web:9c19157b12ade81029be4c",
+  measurementId: "G-4QSLZNN9JK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
